f2a4c8e04a100575efa19eb694e7a2f81d1e3251
AbhisekMandal <134487436+AbhisekMandal@users.noreply.github.com>
2023-11-28 10:21:45 +0000

    introduction javascript file saved

---
+++ 01-basics/test.js
@@ -0,0 +1,8 @@
+console.log("hello world")
+/* 
+1. Js is a high level scripting language to use on developing web and mobile application efficiency.
+2. We use js on client side as well as server side.
+3. In js we do logical implementation in web pages.
+4. In today era we use ESb version of js , as it is latest one.
+
+*/
